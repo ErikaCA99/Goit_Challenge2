80c8a0338eba8764e4fb006ce2f5e473808bc664
Erika Chino <erika.53chinoalanoca@gmail.com>
2026-02-11 15:09:46 -0400

    fix:Correccion de la funcion formatearFecha

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ function formatearFecha(fecha) {
   }
 
   const dia = fechaObj.getDate().toString().padStart(2, "0");
-  const mes = fechaObj.getMonth().toString().padStart(2, "0");
+  const mes = (fechaObj.getMonth() + 1).toString().padStart(2, "0");
   const año = fechaObj.getFullYear();
   const hora = fechaObj.getHours().toString().padStart(2, "0");
   const minutos = fechaObj.getMinutes().toString().padStart(2, "0");
